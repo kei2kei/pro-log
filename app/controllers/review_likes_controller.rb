d9ced57e4a3e5f5a5c9ba7aad71186a281c2bf3d
kei2kei <jianye.hongfu@gmail.com>
2025-12-28 08:37:00 +0900

    コントローラー上で削除時にlikeのidを使用する設計に変更

--- app/controllers/review_likes_controller.rb
+++ app/controllers/review_likes_controller.rb
@@ -1,20 +1,15 @@
 class ReviewLikesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_review
 
   def create
-    current_user.like(@review)
-    redirect_to review_path(@review), notice: "レビューにいいねしました。"
+    review = Review.find(params[:review_id])
+    current_user.like(review)
+    redirect_to review_path(review), notice: "レビューにいいねしました。"
   end
 
   def destroy
-    current_user.unlike(@review)
-    redirect_to review_path(@review), notice: "レビューのいいねを解除しました。"
-  end
-
-  private
-
-  def set_review
-    @review = Review.find(params[:review_id])
+    review = current_user.review_likes.find(params[:id])
+    review.destroy
+    redirect_to review_path(review), notice: "レビューのいいねを解除しました。"
   end
 end
