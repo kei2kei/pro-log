efa762beaa865edc02a57b4fa09e33d407eddea1
kei2kei <jianye.hongfu@gmail.com>
2025-12-27 13:25:09 +0900

    バリデーションの追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+  has_one_attached :image
+
+  enum protein_type: {
+    whey: 0,
+    soy: 1,
+    casein: 2,
+    blend: 3
+  }
+
+  validates :name, :brand, :price, :protein_type, presence: true
 end
