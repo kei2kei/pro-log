3c3a98e842a8177057e81edd503f229bf9644678
kei2kei <jianye.hongfu@gmail.com>
2025-12-28 07:46:59 +0900

    インスタンスメソッドを使用しcreate,destroyアクション作成

--- app/controllers/review_likes_controller.rb
+++ app/controllers/review_likes_controller.rb
@@ -1,7 +1,20 @@
 class ReviewLikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_review
+
   def create
+    current_user.like(@review)
+    redirect_to review_path(@review), notice: "レビューにいいねしました。"
   end
 
   def destroy
+    current_user.unlike(@review)
+    redirect_to review_path(@review), notice: "レビューのいいねを解除しました。"
+  end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:review_id])
   end
 end
