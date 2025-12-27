a1139ba1a9485ad03aaf95f1b76fb9f33614ab4f
kei2kei <jianye.hongfu@gmail.com>
2025-12-27 15:55:50 +0900

    新規作成ページ用コントローラー追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,22 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_product, only: [ :new, :create ]
+
+  def new
+    @review = @product.reviews.build
+  end
+
   def show
     @review = Review.includes(
       user: { avatar_attachment: :blob },
       product: { image_attachment: :blob }
     ).find(params[:id])
     @other_reviews = @review.user.reviews.where.not(id: @review.id).includes(product: { image_attachment: :blob }).limit(4)
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:product_id])
   end
 end
