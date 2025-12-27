ac8d5c9f06b9785c784783a4d4c50f47b0e1c6e0
kei2kei <jianye.hongfu@gmail.com>
2025-12-27 13:24:59 +0900

    マイグレーション完了

--- db/migrate/20251227042105_create_products.rb
+++ db/migrate/20251227042105_create_products.rb
@@ -1,11 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[8.1]
   def change
     create_table :products do |t|
-      t.string :name
-      t.string :brand
-      t.integer :price
+      t.string :name, null: false
+      t.string :brand, null: false
+      t.integer :price, null: false
       t.string :flavor
-      t.integer :protein_type
+      t.integer :protein_type, null: false, default: 0
       t.integer :calorie
       t.integer :protein
       t.integer :fat
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_27_030919) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_27_042105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,6 +40,20 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_27_030919) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "brand", null: false
+    t.integer "calorie"
+    t.integer "carbohydrate"
+    t.datetime "created_at", null: false
+    t.integer "fat"
+    t.string "flavor"
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.integer "protein"
+    t.integer "protein_type", default: 0, null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
