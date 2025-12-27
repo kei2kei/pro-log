391d13a0ee53c2e8b489e3dd8bb3f27740976c56
kei2kei <jianye.hongfu@gmail.com>
2025-12-27 14:48:02 +0900

    制約追加

--- db/migrate/20251227054440_create_reviews.rb
+++ db/migrate/20251227054440_create_reviews.rb
@@ -3,15 +3,15 @@ class CreateReviews < ActiveRecord::Migration[8.1]
     create_table :reviews do |t|
       t.references :user, null: false, foreign_key: true
       t.references :product, null: false, foreign_key: true
-      t.string :title
+      t.string :title, null: false
       t.text :comment
-      t.integer :overall_score
-      t.integer :sweetness
-      t.integer :richness
-      t.integer :aftertaste
-      t.integer :flavor_score
-      t.integer :solubility
-      t.integer :foam
+      t.integer :overall_score, null: false
+      t.integer :sweetness, null: false
+      t.integer :richness, null: false
+      t.integer :aftertaste, null: false
+      t.integer :flavor_score, null: false
+      t.integer :solubility, null: false
+      t.integer :foam, null: false
 
       t.timestamps
     end
