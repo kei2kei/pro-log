6b00cf06a014c9aa1ea3ede5dd4946ba8d96ca21
kei2kei <jianye.hongfu@gmail.com>
2025-12-27 19:37:07 +0900

    一覧画面・詳細画面への非同期ボタンの追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,12 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.with_attached_image.order(created_at: :desc)
+    @bookmarks_by_product_id = current_user.product_bookmarks.index_by(&:product_id)
   end
 
   def show
     @product = Product.with_attached_image.find(params[:id])
     @reviews = @product.reviews.includes(user: { avatar_attachment: :blob }).order(created_at: :desc)
+    @bookmarks_by_product_id = current_user.product_bookmarks.index_by(&:product_id)
   end
 end
