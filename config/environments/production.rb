ea743e89dfa8e3a097ae6c51f2d2abf694e055d1
kei2kei <jianye.hongfu@gmail.com>
2025-12-27 09:01:38 +0900

    ActionCableの記述削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,8 +79,6 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
-  config.solid_cable.enabled = false
-
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
