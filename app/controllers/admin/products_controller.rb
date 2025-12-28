d468b3b9fa5ff8e2d836797aa1ee55c1aa8ac5d3
kei2kei <jianye.hongfu@gmail.com>
2025-12-28 20:19:23 +0900

    管理用商品作成ページ雛形作成

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,8 +1,10 @@
 class Admin::ProductsController < ApplicationController
   def index
+    @products = Product.order(created_at: :desc)
   end
 
   def new
+    @product = Product.new
   end
 
   def create
