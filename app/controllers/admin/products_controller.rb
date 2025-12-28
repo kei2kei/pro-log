8a32106de87c178bd88031264a32c27e0769aa00
kei2kei <jianye.hongfu@gmail.com>
2025-12-28 20:15:33 +0900

    admin用商品コントローラー作成

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,10 @@
+class Admin::ProductsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get "products/index"
+    get "products/new"
+    get "products/create"
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
