a72e514e00778b13c5c710df3c1c1f3b40e41914
kei2kei <jianye.hongfu@gmail.com>
2025-12-28 14:32:36 +0900

    更新アクション追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -24,6 +24,13 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    @user = current_user
+
+    if @user.update(profile_params)
+      redirect_to profile_path, notice: "プロフィールを更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
