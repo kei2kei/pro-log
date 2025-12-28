82b87616a1ae38fb757f4483f1832601a4d0932a
kei2kei <jianye.hongfu@gmail.com>
2025-12-28 14:21:24 +0900

    ユーザーのお気に入り、いいね、レビューを取得し、プロフィール画面に渡す。

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,22 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
+    @user = current_user
+    @tab = params[:tab].presence_in(%w[bookmarks likes reviews]) || "bookmarks"
+    if @tab == "likes"
+      @liked_reviews = Review.includes(
+        user: { avatar_attachment: :blob },
+        product: { image_attachment: :blob }
+      ).joins(:review_likes).where(review_likes: { user_id: @user.id }).distinct
+    elsif @tab == "reviews"
+      @reviewed_products = Product.joins(:reviews)
+                                  .where(reviews: { user_id: @user.id })
+                                  .distinct
+                                  .includes(image_attachment: :blob)
+    else
+      @bookmarked_products = @user.bookmark_products.includes(image_attachment: :blob)
+    end
   end
 
   def edit
