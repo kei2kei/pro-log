2b1e87f394edfec3112ab52359d8589b123fa15d
kei2kei <jianye.hongfu@gmail.com>
2025-12-28 10:39:40 +0900

    タグ保存時の処理についてタグモデルに追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,19 @@ class Tag < ApplicationRecord
   has_many :reviews, through: :review_taggings
   has_many :product_taggings, dependent: :destroy
   has_many :products, through: :product_taggings
+
+  before_validation :normalize_name
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  def self.normalize_names(value)
+    value.to_s.split(/[,\s]+/).map { |name| name.strip.downcase }.reject(&:blank?).uniq
+  end
+
+  private
+
+  # 単体保存用
+  def normalize_name
+    self.name = name.to_s.strip.downcase
+  end
 end
