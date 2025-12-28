9d15b5a4af5f070ed5a939d5006a781369d7b414
kei2kei <jianye.hongfu@gmail.com>
2025-12-28 09:35:48 +0900

    いいいねボタンの非同期化

--- app/controllers/review_likes_controller.rb
+++ app/controllers/review_likes_controller.rb
@@ -2,14 +2,21 @@ class ReviewLikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    review = Review.find(params[:review_id])
-    current_user.like(review)
-    redirect_to review_path(review), notice: "レビューにいいねしました。"
+    @review = Review.find(params[:review_id])
+    current_user.like(@review)
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to review_path(@review), notice: "レビューにいいねしました。" }
+    end
   end
 
   def destroy
-    review = current_user.review_likes.find(params[:id]).review
-    current_user.unlike(review)
-    redirect_to review_path(review), notice: "レビューのいいねを解除しました。"
+    like = current_user.review_likes.find(params[:id])
+    @review = like.review
+    like.destroy
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to review_path(@review), notice: "レビューのいいねを解除しました。" }
+    end
   end
 end
