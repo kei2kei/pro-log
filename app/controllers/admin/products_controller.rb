5d57049c5c5cfd73b8c7739636fe2785625e47f0
kei2kei <jianye.hongfu@gmail.com>
2025-12-28 20:20:51 +0900

    管理者用商品createアクション作成

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -8,5 +8,29 @@ class Admin::ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to admin_products_path, notice: "商品を登録しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :brand,
+      :protein_type,
+      :flavor,
+      :price,
+      :calorie,
+      :protein,
+      :fat,
+      :carbohydrate,
+      :image
+    )
   end
 end
