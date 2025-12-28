1bfe87a0f710e6614b8138f8728f29723af6f073
kei2kei <jianye.hongfu@gmail.com>
2025-12-28 20:34:01 +0900

    管理者用商品更新、削除アクション追加

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,8 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_admin!
+  before_action :set_product, only: [:edit, :update, :destroy]
+
   def index
     @products = Product.order(created_at: :desc)
   end
@@ -17,20 +21,37 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to admin_products_path, notice: "商品を更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path, alert: "商品を削除しました。"
+  end
+
   private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(
-      :name,
-      :brand,
-      :protein_type,
-      :flavor,
-      :price,
-      :calorie,
-      :protein,
-      :fat,
-      :carbohydrate,
+      :name, :brand, :flavor, :protein_type,
+      :price, :calorie, :protein, :fat, :carbohydrate,
       :image
     )
+  end
+
+  def require_admin!
+    redirect_to root_path, alert: "権限がありません。" unless current_user.admin?
   end
 end
