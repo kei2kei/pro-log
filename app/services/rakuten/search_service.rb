784b93decea073437c8fbd6b50032846ff2afd3e
kei2kei <jianye.hongfu@gmail.com>
2025-12-30 15:33:00 +0900

    API取得処理の修正

--- app/services/rakuten/search_service.rb
+++ app/services/rakuten/search_service.rb
@@ -1,17 +1,19 @@
-class RakutenSearchService
-  def self.search_products(keyword:)
-    RakutenWebService::Ichiba::Item.search(
-      keyword: keyword,
-      hits: 20,
-      availability: 1
-    ).map do |item|
-      {
-        name: item["itemName"],
-        url: item["itemUrl"],
-        image_url: item["mediumImageUrls"]&.first&.dig("imageUrl"),
-        price: item["itemPrice"],
-        shop: item["shopName"]
-      }
+module Rakuten
+  class SearchService
+    def self.search_products(keyword:)
+      RakutenWebService::Ichiba::Item.search(
+        keyword: keyword,
+        hits: 20,
+        availability: 1
+      ).map do |item|
+        {
+          name: item["itemName"],
+          url: item["itemUrl"],
+          image_url: item["mediumImageUrls"]&.first,
+          price: item["itemPrice"],
+          shop: item["shopName"]
+        }
+      end
     end
   end
 end
