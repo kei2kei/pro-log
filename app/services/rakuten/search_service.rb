e0cad7a368e6e299459d4882049b8503243da117
kei2kei <jianye.hongfu@gmail.com>
2025-12-30 14:51:32 +0900

    商品検索機能追加

---
+++ app/services/rakuten/search_service.rb
@@ -0,0 +1,17 @@
+class RakutenSearchService
+  def self.search_products(keyword:)
+    RakutenWebService::Ichiba::Item.search(
+      keyword: keyword,
+      hits: 20,
+      availability: 1
+    ).map do |item|
+      {
+        name: item["itemName"],
+        url: item["itemUrl"],
+        image_url: item["mediumImageUrls"]&.first&.dig("imageUrl"),
+        price: item["itemPrice"],
+        shop: item["shopName"]
+      }
+    end
+  end
+end
