800c1ce409fdb5396119ab236dcae5f0fb2bf87d
kei2kei <jianye.hongfu@gmail.com>
2026-01-03 21:15:51 +0900

    URL安全化ヘルパー追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def safe_external_url(raw_url)
+    return if raw_url.blank?
+
+    uri = URI.parse(raw_url.to_s)
+    return unless uri.is_a?(URI::HTTP) && uri.host.present?
+
+    uri.to_s
+  rescue URI::InvalidURIError
+    nil
+  end
 end
