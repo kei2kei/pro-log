ac4c369cfa742a8288f755cbf223a885b47d449b
kei2kei <jianye.hongfu@gmail.com>
2026-01-11 21:04:57 +0900

    DOMAINの削除とポートの固定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,10 +63,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV.fetch("SMTP_ADDRESS"),
-    port: ENV.fetch("SMTP_PORT", "587").to_i,
+    port: 587,
     user_name: ENV.fetch("SMTP_USERNAME"),
     password: ENV.fetch("SMTP_PASSWORD"),
-    domain: ENV.fetch("SMTP_DOMAIN"),
     authentication: :plain,
     enable_starttls_auto: true
   }
