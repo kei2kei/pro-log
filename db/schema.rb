afd92def9cf36000ef6357f8b3f19cfe6a6d8a4d
kei2kei <jianye.hongfu@gmail.com>
2026-01-11 18:53:26 +0900

    一意制約の追加

--- db/schema.rb
+++ db/schema.rb
@@ -141,6 +141,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_11_084645) do
     t.string "username", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
