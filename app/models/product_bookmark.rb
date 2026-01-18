23430ab1f7c05f18858a424c2c2cbbe17d2d56bf
kei2kei <jianye.hongfu@gmail.com>
2026-01-18 12:01:31 +0900

    ブックマーク時やレビュー時に平均値計算をジョブとしてキューに入れる

--- app/models/product_bookmark.rb
+++ app/models/product_bookmark.rb
@@ -3,4 +3,12 @@ class ProductBookmark < ApplicationRecord
   belongs_to :product
 
   validates :user_id, uniqueness: { scope: :product_id }
+
+  after_commit :enqueue_product_stats_refresh, on: %i[create destroy]
+
+  private
+
+  def enqueue_product_stats_refresh
+    ProductStatRefreshJob.perform_later(product_id)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -11,6 +11,8 @@ class Review < ApplicationRecord
             numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :user_id, uniqueness: { scope: :product_id }
 
+  after_commit :enqueue_product_stats_refresh, on: %i[create update destroy]
+
   def tag_names
     # フォーム表示用にキャッシュ格納
     @tag_names ||= tags.pluck(:name).join(" ")
@@ -29,5 +31,11 @@ class Review < ApplicationRecord
 
   def self.ransackable_associations(auth_object = nil)
     %w[product]
+  end
+
+  private
+
+  def enqueue_product_stats_refresh
+    ProductStatRefreshJob.perform_later(product_id)
   end
 end
