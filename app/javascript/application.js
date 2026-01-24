0dd628a08900860f473c4ff3db56dfde981a924d
kei2kei <jianye.hongfu@gmail.com>
2026-01-24 11:41:52 +0900

    イベントリスナー定義

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+document.addEventListener("turbo:visit", () => {
+  document.documentElement.dataset.loading = "true"
+})
+
+document.addEventListener("turbo:submit-start", () => {
+  document.documentElement.dataset.loading = "true"
+})
+
+document.addEventListener("turbo:load", () => {
+  document.documentElement.dataset.loading = "false"
+})
+
+document.addEventListener("turbo:submit-end", () => {
+  document.documentElement.dataset.loading = "false"
+})
