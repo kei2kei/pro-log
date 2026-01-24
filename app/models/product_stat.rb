7f641470cee1724d7917f68c10b7836f3b49839b
kei2kei <jianye.hongfu@gmail.com>
2026-01-24 09:40:00 +0900

    クエリ発行の効率化

--- app/models/product_stat.rb
+++ app/models/product_stat.rb
@@ -4,16 +4,28 @@ class ProductStat < ApplicationRecord
   def self.refresh_for(product)
     product_id = product.is_a?(Product) ? product.id : product
     reviews = Review.where(product_id: product_id)
+    aggregates = reviews
+      .select(
+        "AVG(overall_score) AS avg_overall_score",
+        "AVG(sweetness) AS avg_sweetness",
+        "AVG(richness) AS avg_richness",
+        "AVG(aftertaste) AS avg_aftertaste",
+        "AVG(flavor_score) AS avg_flavor_score",
+        "AVG(solubility) AS avg_solubility",
+        "AVG(foam) AS avg_foam",
+        "COUNT(*) AS reviews_count"
+      )
+      .take
 
     stats = find_or_initialize_by(product_id: product_id)
-    stats.avg_overall_score = reviews.average(:overall_score)
-    stats.avg_sweetness = reviews.average(:sweetness)
-    stats.avg_richness = reviews.average(:richness)
-    stats.avg_aftertaste = reviews.average(:aftertaste)
-    stats.avg_flavor_score = reviews.average(:flavor_score)
-    stats.avg_solubility = reviews.average(:solubility)
-    stats.avg_foam = reviews.average(:foam)
-    stats.reviews_count = reviews.count
+    stats.avg_overall_score = aggregates.read_attribute(:avg_overall_score)
+    stats.avg_sweetness = aggregates.read_attribute(:avg_sweetness)
+    stats.avg_richness = aggregates.read_attribute(:avg_richness)
+    stats.avg_aftertaste = aggregates.read_attribute(:avg_aftertaste)
+    stats.avg_flavor_score = aggregates.read_attribute(:avg_flavor_score)
+    stats.avg_solubility = aggregates.read_attribute(:avg_solubility)
+    stats.avg_foam = aggregates.read_attribute(:avg_foam)
+    stats.reviews_count = aggregates.read_attribute(:reviews_count)
     stats.bookmarks_count = ProductBookmark.where(product_id: product_id).count
     stats.save!
   end
