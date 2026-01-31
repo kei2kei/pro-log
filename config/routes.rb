52cc6869857419384bb3fccf19f015415d41d421
kei2kei <jianye.hongfu@gmail.com>
2026-01-31 12:34:39 +0900

    オートコンプリートルートの作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+
+  get "autocomplete/tags", to: "autocomplete#tags"
+  get "autocomplete/search", to: "autocomplete#search"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
