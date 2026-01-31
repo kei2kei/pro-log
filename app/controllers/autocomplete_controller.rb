7770c5d69d7f354e15421f05d31b973043e334cd
kei2kei <jianye.hongfu@gmail.com>
2026-01-31 12:34:10 +0900

    クエリを1本化

--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -14,11 +14,22 @@ class AutocompleteController < ApplicationController
     query = params[:q].to_s.strip
     return render json: [] if query.blank?
 
-    names = Product.where("name ILIKE ?", "%#{query}%").limit(10).pluck(:name)
-    brands = Product.where("brand ILIKE ?", "%#{query}%").limit(10).pluck(:brand)
-    flavors = Product.where("flavor ILIKE ?", "%#{query}%").limit(10).pluck(:flavor)
-    tags = Tag.where("name ILIKE ?", "%#{query}%").limit(10).pluck(:name)
+    pattern = "%#{query}%"
+    sql = <<~SQL.squish
+      (SELECT name AS value FROM products WHERE name ILIKE :pattern LIMIT 10)
+      UNION
+      (SELECT brand AS value FROM products WHERE brand ILIKE :pattern LIMIT 10)
+      UNION
+      (SELECT flavor AS value FROM products WHERE flavor ILIKE :pattern LIMIT 10)
+      UNION
+      (SELECT name AS value FROM tags WHERE name ILIKE :pattern LIMIT 10)
+      LIMIT 20
+    SQL
 
-    render json: (names + brands + flavors + tags).uniq.first(20)
+    results = ActiveRecord::Base.connection.exec_query(
+      ActiveRecord::Base.send(:sanitize_sql_array, [sql, { pattern: pattern }])
+    )
+
+    render json: results.rows.flatten
   end
 end
