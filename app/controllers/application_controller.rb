16b10cdeb43c2d0e01d8306a5a516451b02d1d68
kei2kei <jianye.hongfu@gmail.com>
2026-01-31 16:04:00 +0900

    例外発生時のページ遷移追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,30 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  unless Rails.env.development? || Rails.env.test?
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    rescue_from ActionController::InvalidAuthenticityToken, with: :render_422
+    rescue_from StandardError, with: :render_500
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :avatar ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :username, :avatar ])
+  end
+
+  private
+
+  def render_404
+    render file: Rails.root.join("public/404.html"), status: :not_found, layout: false
+  end
+
+  def render_422
+    render file: Rails.root.join("public/422.html"), status: :unprocessable_content, layout: false
+  end
+
+  def render_500
+    render file: Rails.root.join("public/500.html"), status: :internal_server_error, layout: false
   end
 end
