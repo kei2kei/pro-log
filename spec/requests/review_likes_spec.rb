d0821fedb44afa6f93e1abc7159f5c657f67c019
kei2kei <jianye.hongfu@gmail.com>
2026-02-01 12:02:30 +0900

    いいねとブックマークのリクエストテスト作成

--- spec/requests/review_likes_spec.rb
+++ spec/requests/review_likes_spec.rb
@@ -1,17 +1,42 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "ReviewLikes", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/review_likes/create"
-      expect(response).to have_http_status(:success)
+  describe "POST /likes" do
+    it "いいねが作成できる" do
+      user = create(:user)
+      review = create(:review)
+      sign_in user
+
+      expect {
+        post likes_path, params: { review_id: review.id }
+      }.to change(ReviewLike, :count).by(1)
+
+      expect(response).to redirect_to(review_path(review))
+    end
+
+    it "自分のレビューにはいいねできない" do
+      user = create(:user)
+      review = create(:review, user: user)
+      sign_in user
+
+      expect {
+        post likes_path, params: { review_id: review.id }, headers: { "HTTP_REFERER" => review_path(review) }
+      }.not_to change(ReviewLike, :count)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/review_likes/destroy"
-      expect(response).to have_http_status(:success)
+  describe "DELETE /likes/:id" do
+    it "いいねを解除できる" do
+      user = create(:user)
+      review = create(:review)
+      like = create(:review_like, user: user, review: review)
+      sign_in user
+
+      expect {
+        delete like_path(like), headers: { "HTTP_REFERER" => review_path(review) }
+      }.to change(ReviewLike, :count).by(-1)
+
+      expect(response).to redirect_to(review_path(review))
     end
   end
 end
