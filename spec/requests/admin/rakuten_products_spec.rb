e4fb8646c83ccb80fa709026710d68ff8df8c6f7
kei2kei <jianye.hongfu@gmail.com>
2026-02-06 11:07:20 +0900

    rakutenAPI部分のテスト作成

--- spec/requests/admin/rakuten_products_spec.rb
+++ spec/requests/admin/rakuten_products_spec.rb
@@ -1,7 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Admin::RakutenProducts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:admin) { create(:user, admin: true) }
+
+  before do
+    sign_in admin
+  end
+
+  describe "GET /admin/rakuten_products/search" do
+    it "検索ページを表示できる" do
+      get search_admin_rakuten_products_path
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
