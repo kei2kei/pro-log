5727a06c1219ca4b186fd20bd776da77b1fe8eb2
kei2kei <jianye.hongfu@gmail.com>
2026-02-11 10:39:53 +0900

    3件追加時の非有効化をやめメッセージ表示に切り替え（turboにしたことで全ボタンの再描画不可のため）

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -4,12 +4,28 @@ class ComparisonsController < ApplicationController
   def add
     @product = Product.find(params[:product_id])
     ids = compared_product_ids
-    ids << @product.id if !ids.include?(@product.id) && ids.size < 3
+    added = false
+
+    if !ids.include?(@product.id) && ids.size < 3
+      ids << @product.id
+      added = true
+    end
+
     session[:compare_product_ids] = ids
 
     respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_back fallback_location: products_path }
+      format.turbo_stream do
+        if !added && !ids.include?(@product.id)
+          flash.now[:alert] = t("shared.compare.limit_alert")
+        end
+      end
+      format.html do
+        if !added && !ids.include?(@product.id)
+          redirect_back fallback_location: products_path, alert: t("shared.compare.limit_alert")
+        else
+          redirect_back fallback_location: products_path
+        end
+      end
     end
   end
 
