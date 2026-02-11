970bbfa584f492c4bc62f6fbcebb0212d0bc60ee
kei2kei <jianye.hongfu@gmail.com>
2026-02-11 10:01:03 +0900

    トレイと比較追加ボタンのTurbo化

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -2,18 +2,27 @@ class ComparisonsController < ApplicationController
   before_action :authenticate_user!
 
   def add
-    product = Product.find(params[:product_id])
+    @product = Product.find(params[:product_id])
     ids = compared_product_ids
-    ids << product.id unless ids.include?(product.id)
+    ids << @product.id unless ids.include?(@product.id)
     session[:compare_product_ids] = ids
-    redirect_back fallback_location: products_path
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: products_path }
+    end
   end
 
   def remove
+    @product = Product.find_by(id: params[:product_id])
     ids = compared_product_ids
     ids.delete(params[:product_id].to_i)
     session[:compare_product_ids] = ids
-    redirect_to params[:return_to].presence || compare_path
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to params[:return_to].presence || compare_path }
+    end
   end
 
   def clear
