6bc41f3bc119619cc111ee02092f19b1e59bbbfd
kei2kei <jianye.hongfu@gmail.com>
2026-02-14 11:45:43 +0900

    レビュー詳細画面の未ログイン公開

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [ :show ]
   before_action :set_product, only: [ :new, :create ]
   before_action :set_review, only: [ :show, :edit, :update, :destroy ]
   before_action :authorize_review!, only: [ :edit, :update, :destroy ]
