805d329f946421450c8e6e972797e314a870f5c3
kei2kei <jianye.hongfu@gmail.com>
2026-02-14 22:47:27 +0900

    api_keyの呼び出し方の修正

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,4 +1,6 @@
 # config/initializers/openai.rb
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.openai[:api_key]
+  config.access_token =
+    Rails.application.credentials.dig(:openai, :api_key).presence ||
+    ENV["OPENAI_API_KEY"].to_s.presence
 end
