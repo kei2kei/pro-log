703130e3a53117aee6dcb2008bff2e9a41e4e4a7
kei2kei <jianye.hongfu@gmail.com>
2026-02-14 22:14:47 +0900

    テスト時のログインロジックの修正

--- spec/support/system_helpers.rb
+++ spec/support/system_helpers.rb
@@ -4,7 +4,7 @@ module SystemHelpers
     fill_in I18n.t("devise.views.sessions.new.email_label"), with: user.email
     fill_in I18n.t("devise.views.sessions.new.password_label"), with: password
     click_button I18n.t("devise.views.sessions.new.submit")
-    expect(page).to have_content(I18n.t("shared.header.logout"))
+    expect(page).to have_link(I18n.t("shared.header.logout"), visible: :all)
   end
 end
 
